e7b848c0a409a454ec9a2b4e03c73017b6e68bfc
Doctor-Andonuts <doctor-andonuts@users.noreply.github.com>
2015-05-04 13:30:57 -0400

    When items are dropped close it prints the item name to log file

--- objects/wired/energy/coalgenerator/coalgenerator.lua
+++ objects/wired/energy/coalgenerator/coalgenerator.lua
@@ -1,14 +1,29 @@
 function init()
-	entity.setInteractive(true)
+	--entity.setInteractive(true)
+	
+	local pos = entity.position()
+	self.itemPickupArea = {
+		{pos[1] - 1, pos[2] - 1},
+		{pos[1] + 1, pos[2]}
+	}
 end
 
-function onInteraction(args)
-	world.logInfo("Test")
+function getFuelItems()
+	local dropIds = world.itemDropQuery(self.itemPickupArea[1], self.itemPickupArea[2])
+	for i, entityId in ipairs(dropIds) do
+		local itemName = world.entityName(entityId)
+		--local item = world.takeItemDrop(entityId)
+		world.logInfo(itemName)
+	end
+end
+
+function update(dt)
+	getFuelItems()
 end
 
 --[[
-function update(dt)
-
+function onInteraction(args)
+	world.logInfo("Test")
 end
 
 function uninit()
